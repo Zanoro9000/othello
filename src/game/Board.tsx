18a08f49b3e8133ad16638b8689fd33c590d02c5
Tyler Kennedy <tyken628@gmail.com>
2022-04-10 01:12:19 -0400

    cleanup unnecessary component

--- src/game/Board.tsx
+++ src/game/Board.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useGameSelector } from '../redux/slices/gameSlice';
-import { GridSpace } from './GridSpace';
 import './Board.scss';
+import { Tile } from './Tile';
 
 export function Board() {
-  const { rows, gameState } = useGameSelector((s) => s);
-
+  const gameState = useGameSelector((s) => s.gameState);
   return (
-    <div className="gameBoard" style={{ gridTemplateRows: `repeat(${rows}, 1fr)` }}>
+    <div className="gameBoard" style={{ gridTemplateRows: `repeat(${gameState.length}, 1fr)` }}>
       {gameState.map((gr, r) => (
         <div key={`row_${r}`} className="gridRow">
-          {gr.map((tile, c) => <GridSpace key={`space_${r}x${c}`} tile={tile} />)}
+          {gr.map((tile, c) => (
+            <div key={`space_${r}x${c}`} className="gridSpace">
+              {tile?.type !== undefined && <Tile key={`tile_${r}x${c}`} row={r} col={c} type={tile?.type} />}
+            </div>
+          ))}
         </div>
       ))}
     </div>
--- src/game/Tile.tsx
+++ src/game/Tile.tsx
@@ -17,27 +17,23 @@ const getTileClassName = (type: TILE_COLOR): TileClassNames => {
   }
 };
 
-export type TileProps = {
-  tile: GamePiece,
-}
-
-export function Tile({ tile }: TileProps) {
+export function Tile({ row, col, type }: GamePiece) {
   const dispatch = useAppDispatch();
   const { startingPlayer, turn } = useAppSelector((s) => ({ startingPlayer: s.game.startingPlayer, turn: s.game.turn }));
 
   const onClick = () => {
-    dispatch(placePiece({ ...tile, type: getTileColor(turn, startingPlayer) }));
+    dispatch(placePiece({ row, col, type: getTileColor(turn, startingPlayer) }));
   };
 
   // startingPlayer is enum of color, so either 0 or 1
   const turnColor: TILE_COLOR = turn % 2 === startingPlayer ? startingPlayer : 1 - startingPlayer;
 
   // button for accessibility
-  const Component = tile.type === TILE_COLOR.AVAILABLE
+  const Component = type === TILE_COLOR.AVAILABLE
     ? (props: React.HTMLAttributes<HTMLButtonElement>) => <button onClick={onClick} {...props} />
     : (props: React.HTMLAttributes<HTMLDivElement>) => <div {...props} />;
 
-  return <Component className={`tile ${getTileClassName(tile.type)} turn-${getTileClassName(turnColor)}`} />;
+  return <Component className={`tile ${getTileClassName(type)} turn-${getTileClassName(turnColor)}`} />;
 }
 
 export type StaticTileProps = {
